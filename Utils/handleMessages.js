50457bdf9517fc3e5e9aacf1158160f83271f0e4
xSaadRM <saadrm.dev@gmail.com>
2024-07-15 16:53:12 +0100

    .

--- Utils/handleMessages.js
+++ Utils/handleMessages.js
@@ -2,11 +2,11 @@ import getServersList from "../Actions/serversList.js";
 import { client, botOwner } from "../index.js";
 
 const handleMessages = async (message) => {
-  const msg = message?.body?.toLowerCase();
+  const body = message?.body?.toLowerCase();
   const fromGrp = message.isGroupMsg;
   const sender = message.from;
 
-  switch (msg) {
+  switch (body) {
     case "hi":
       client.reply(sender, "sup", message.id);
       break;
@@ -14,16 +14,13 @@ const handleMessages = async (message) => {
       client.reply(sender, await getServersList(), message.id);
       break;
     default:
-      fromGrp ? console.log(message) : null;
-      console.log(msg.mentionedJidList);
-
       switch (true) {
-        // case msg.mentionedJidList.includes("212665715774"):
-        //   client.sendPhotoVideoViaTyping(
-        //     sender,
-        //     "https://www.youtube.com/watch?v=OO8c6EAePBs"
-        //   );
-        //   break;
+        case message.mentionedJidList.includes("212665715774"):
+          client.sendPhotoVideoViaTyping(
+            sender,
+            "https://www.youtube.com/watch?v=OO8c6EAePBs"
+          );
+          break;
 
         default:
           break;
